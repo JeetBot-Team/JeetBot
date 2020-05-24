0a2bf738dee764cc7da0553a14f0f8fd007351ef
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-05-24 17:02:31 +0900

    Added Error Handling on kick and ban

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,19 +1,20 @@
 module.exports = async (msg) => {
 
-    /*
-        Future to add:
-          Error handling to check if user has permissions to use this
-    */
+    if(msg.member.hasPermission(['BAN_MEMBERS'])) {
+        console.log(`${msg.author.username} can ban people from Discord Server: ${msg.guild}`);
+        if (!msg.mentions.users.size) {
+            console.log('Moderator did not tag a user when using ban function')
+            return msg.channel.send('You need to tag a user in order to ban them!');
+        }
 
-    if (!msg.mentions.users.size) {
-        console.log('Moderator did not tag a user when using ban function')
-        return msg.channel.send('You need to tag a user in order to ban them!');
-    }
+        const taggedUser = msg.mentions.users.first();
 
-    const taggedUser = msg.mentions.users.first();
+        msg.channel.send(`You banned ${taggedUser.username}`);
+        msg.guild.members.ban(taggedUser);
 
-    msg.channel.send(`You banned ${taggedUser.username}`);
-    msg.guild.members.ban(taggedUser);
-
-    console.log('Moderator has banned someone!');
+        console.log(`Moderator ${msg.author.username} has banned ${taggedUser} from ${msg.guild}!`);
+    } else {
+        console.log('This member cannot ban members');
+        return msg.channel.send(`${msg.author.username} do not have the authority to ban someone`);
+    }    
 };
