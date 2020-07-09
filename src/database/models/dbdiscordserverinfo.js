cb1e02a99e1d456af2f0e6f2d4d248807c671869
softwaredeveloptam <39637317+softwaredeveloptam@users.noreply.github.com>
2020-07-09 23:03:26 +0900

    Added DB information and did some error handling for checking if servers are on DB

---
+++ src/database/models/dbdiscordserverinfo.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const DiscordServerInfoSchema = new mongoose.Schema({
+	server_id: {
+		type: String
+	},
+	server_name: {
+		type: String
+	},
+	discord_owner_id: {
+		type: Number,  
+	},
+	discord_username: {
+		type: String
+	},
+	discord_user_token: {
+		type: String
+	},
+	last_login: {
+		type: String
+	},
+    RoleReactions: { 
+		Message_ID: Number,
+		RoleMappings: {
+			type: mongoose.Schema.Types.Mixed
+		}			 
+	},
+});
+
+
+const DiscordServerInfoModel = module.exports = mongoose.model('DiscordServerInfo', DiscordServerInfoSchema);
--- src/index.js
+++ src/index.js
@@ -1,28 +1,69 @@
+// Discord Client
 const Discord = require('discord.js');
+const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION']});
+
+// dotenv
 require('dotenv').config();
 
-const commandHandler = require('./commands'); // looks for index.js
+// MongoDB Info
+const database = require('./database/database');
+const mongoose = require('mongoose');
+const ServerInfo = require('./database/models/dbdiscordserverinfo');
 
-const client = new Discord.Client({ partials: ['MESSAGE', 'REACTION']});
-const cooldowns = new Discord.Collection(); // work on cooldowns later
+// Handlers
+const commandHandler = require('./commands');
+
+// cooldowns to fix later
+const cooldowns = new Discord.Collection(); 
 
 client.once('ready', () => {
-	console.log(`${client.user.tag} is Ready To Rock And Roll!`);
+    console.log(`${client.user.tag} is Ready To Rock And Roll!`);
+    database.then(() => console.log(`${client.user.tag} is connected to MongoDB.`)).catch(err => console.log(err));
+
+    client.guilds.cache.map(async (guild) => {
+
+        let guildInfo = await ServerInfo.findOne({
+            server_id: guild.id,
+        });
+
+        if(!guildInfo) {
+            let serverInfo = new ServerInfo({
+                server_id: guild.id,
+                server_name: guild.name,
+                discord_owner_id: guild.ownerID,
+            })
+            
+            try {
+                await serverInfo.save();
+                console.log(`${guild.name} has been saved to the Database`);
+            } catch {
+                err => console.log(err);
+            }
+
+        } else {
+            console.log(`${guild.name} exists in the Database`);
+        }
+    });
+    
+    // console.log(`${client.user.tag} has added all the server he's on to the database.`);
+
 });
+
+
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
     // Send the message to a designated channel on a server:
     const channel = member.guild.channels.cache.find(ch => ch.name === 'general');
     // Do nothing if the channel wasn't found on this server
+    // console.log(member);
     if (!channel) return;
     // Send the message, mentioning the member
     channel.send(`Welcome to the Coding Hell Gang ${member}!`);
     channel.send('Please select a role in #announcements by clicking the chef or wow emoji');
 });
 
-const roleSelectMessageId = '719170733007437886';
-
+const roleSelectMessageId = '708954046207098881';
 // Adds a Role to user when user uses reaction on role-select
 client.on('messageReactionAdd', async (reaction, user) => {
 
